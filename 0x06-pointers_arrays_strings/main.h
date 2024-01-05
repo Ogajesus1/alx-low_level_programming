d6a5b58ebcf1be660e56598801cca24f0123e7dd
Ogajesus1 <eziheconfidence@gmail.com>
2024-01-05 20:53:47 +0000

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-ifndef MAIN.H
-define MAIN.H
+#ifndef MAIN.H
+#define MAIN.H
 int _putchar(char c);
 char "_strcat(char "dest, char"src);
 char "_strcat(char "dest, char"src,int n);
@@ -13,4 +13,4 @@ char "rot 13(char "a);
 void print_number(int n);
 char *infinite_add(char "n1, char "n2, char "r, int size_r);
 void print_buffer(char "b, int size);
-exit
+#ifndef
